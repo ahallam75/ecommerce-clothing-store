efafbfa2c2d54175a226a10dae0ce172b9bbfa76
ahallam <alexhallam75@gmail.com>
2022-12-08 15:35:17 -0500

    Updated utils

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -21,12 +21,12 @@ import {
 } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyAS8XiBPni8i0A_RcR2_u8QbiuWapWNnrk",
+  authDomain: "ecommerce-clothing-store-db.firebaseapp.com",
+  projectId: "ecommerce-clothing-store-db",
+  storageBucket: "ecommerce-clothing-store-db.appspot.com",
+  messagingSenderId: "326039015901",
+  appId: "1:326039015901:web:12dff1969459867a15e634"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
