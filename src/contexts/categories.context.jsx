7c576f1ae8b53ab407df47271fd8fa86392f7a50
ahallam <alexhallam75@gmail.com>
2022-12-09 08:35:24 -0500

    Updated collectionRef from 'categories' to 'collections' to match name in Firestore

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -12,6 +12,7 @@ export const CategoriesProvider = ({ children }) => {
   useEffect(() => {
     const getCategoriesMap = async () => {
       const categoryMap = await getCategoriesAndDocuments('categories');
+      console.log('Clothing Data', categoryMap)
       setCategoriesMap(categoryMap);
     };
 
